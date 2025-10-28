a876cb1d1074ab999c08af01652f714d19149992
Saurlax <saurlax@qq.com>
2025-10-28 22:29:22 +0800

    feat: add cas service url env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
       "Sci-tech innovation system for contests, awards and applications.",
     casBaseUrl: "https://sso.dlut.edu.cn/cas",
     casService:
+      process.env.CAS_SERVICE_URL ||
       "https%3A%2F%2Fwebvpn.dlut.edu.cn%2Fhttp-3000%2F57787a7876706e323032336b6579402474170119f00b9c5dbe%2Fapi%2Fcas",
   },
   ui: {
