820a3086e9291e71233521412786020f97c28c97
Saurlax <saurlax@qq.com>
2026-03-05 12:23:25 +0800

    feat: move app config into app folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,16 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxtjs/mdc", "nuxt-auth-utils", "@nuxthub/core"],
-  appConfig: {
-    site: "https://cippus.naosi.org",
-    title: "创新创业实践中心",
-    description:
-      "Sci-tech innovation system for contests, awards and applications.",
-    casBaseUrl: process.env.CAS_BASE_URL ?? "https://sso.dlut.edu.cn/cas",
-    casServiceUrl:
-      process.env.CAS_SERVICE_URL ??
-      "https%3A%2F%2Fwebvpn.dlut.edu.cn%2Fhttp-3000%2F57787a7876706e323032336b6579402474170119f00b9c5dbe%2Fapi%2Fcas",
-  },
   ui: {
     fonts: false,
   },
